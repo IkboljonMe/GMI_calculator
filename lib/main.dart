0f4ef74db4b8c9bad22620705c620fa77cefad03
IkboljonMe <ikboljonme@gmail.com>
2022-09-18 02:36:48 +0200

    Lib/main.dart is modified

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'BMI',
+      title: 'BMI CALCULATOR',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.yellow,
